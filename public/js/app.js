00fd2c6ba45091bfe98a645d40ea1151449ff9a3
David Lumm <david.lumm@twinklebob.co.uk>
2020-04-15 16:40:03 +0100

    Add date content to page
    Unstyled so far, but a start to delivering the core aims

--- public/js/app.js
+++ public/js/app.js
@@ -19283,6 +19283,44 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+function ready(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+} // Borrowed from https://stackoverflow.com/a/15397495/1016336
+
+
+var nth = function nth(d) {
+  if (d > 3 && d < 21) return 'th';
+
+  switch (d % 10) {
+    case 1:
+      return "st";
+
+    case 2:
+      return "nd";
+
+    case 3:
+      return "rd";
+
+    default:
+      return "th";
+  }
+};
+
+ready(function () {
+  // Get todays date and fill the required cards
+  var today = new Date();
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  document.getElementById('day').innerHTML = days[today.getDay()];
+  document.getElementById('dom').innerHTML = today.getDate() + nth(today.getDate());
+  document.getElementById('month').innerHTML = months[today.getMonth()];
+  document.getElementById('year').innerHTML = today.getFullYear();
+});
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
 require('./bootstrap');
+
+function ready(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+// Borrowed from https://stackoverflow.com/a/15397495/1016336
+const nth = function(d) {
+  if (d > 3 && d < 21) return 'th';
+  switch (d % 10) {
+    case 1:  return "st";
+    case 2:  return "nd";
+    case 3:  return "rd";
+    default: return "th";
+  }
+}
+
+ready(function() {
+    // Get todays date and fill the required cards
+
+    let today = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
+    document.getElementById('day').innerHTML = days[today.getDay()];
+
+    document.getElementById('dom').innerHTML = today.getDate() + nth(today.getDate());
+    document.getElementById('month').innerHTML = months[today.getMonth()];
+    document.getElementById('year').innerHTML = today.getFullYear();
+});
